c0bdfaa4fb2b1bae699836cc90717929f8b1c337
Elias Marin <eliasmarinh2021@gmail.com>
2023-04-20 19:11:02 -0300

    Arreglo de errores

--- inserts.sql
+++ inserts.sql
@@ -1,12 +1,5 @@
 use CLIENTES;
 
-INSERT INTO CLIENTES (CLI_ID, CLI_NOMBRE, CLI_APELLIDO, CLI_CUIT, CLI_DIRECCION, CLI_LOC_ID, CLI_RAZONSOC)
-VALUES 
-    (1, 'Juan', 'Pérez', '20-12345678-9', 'Calle Falsa 123', 'AR-BA', 'Compañía A'),
-    (2, 'María', 'García', '27-34567890-1', 'Avenida Siempreviva 742', 'AR-C', 'Compañía B'),
-    (3, 'Pedro', 'González', '23-98765432-1', 'Calle Falsa 456', 'AR-B', 'Compañía C'),
-    (4, 'Lucía', 'Rodríguez', '30-87654321-0', 'Calle Real 789', 'AR-C', 'Compañía D');
-
 
 INSERT INTO PROVINCIAS (PRO_NOMBRE) VALUES
 ("Buenos Aires"),
@@ -48,6 +41,12 @@ INSERT INTO LOCALIDADES (LOC_NOMBRE,LOC_PRO_ID) VALUES
 ('Bahía Blanca',1),
 ('Balcarce',1);
 
+INSERT INTO CLIENTES (CLI_ID, CLI_NOMBRE, CLI_APELLIDO, CLI_CUIT, CLI_DIRECCION, CLI_LOC_ID, CLI_RAZONSOC)
+VALUES 
+    (1, 'Juan', 'Pérez', '20-12345678-9', 'Calle Falsa 123', 'AR-BA', 'Compañía A'),
+    (2, 'María', 'García', '27-34567890-1', 'Avenida Siempreviva 742', 'AR-C', 'Compañía B'),
+    (3, 'Pedro', 'González', '23-98765432-1', 'Calle Falsa 456', 'AR-B', 'Compañía C'),
+    (4, 'Lucía', 'Rodríguez', '30-87654321-0', 'Calle Real 789', 'AR-C', 'Compañía D');
 
 UPDATE CLIENTES SET CLI_NOMBRE = 'Laura', CLI_APELLIDO = 'Fernández' WHERE CLI_ID = 2;
 
